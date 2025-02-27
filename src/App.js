73325691ac1d409d57863ec88cf8eaac87ef6faf
selvazvin <selvazvin@gmail.com>
2025-02-27 11:30:50 +0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar'
+import About from "./components/About"
+import Product from "./components/Products"
+import Search from "./components/Search"
+import Footer from "./components/Footer"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Navbar></Navbar>
+            <Search></Search>
+            <Product></Product>
+            <About></About>
+            <Footer></Footer>
+        </>
+    )
 }
 
-export default App;
+export default App 
